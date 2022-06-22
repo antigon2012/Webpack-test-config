a18b2c2d7c233cd5e16928f797e798e7203868c6
Cstrp <valery.nosareu@gmail.com>
2022-06-22 16:02:07 +0300

    test

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   trailingComma: 'es5',
-  tabWidth: 2, // настройка табов (отступов)
+  tabWidth: 4, // настройка табов (отступов)
   semi: false, // отключение точек с запятыми
   singleQuote: true, // одинарные кавычки
   printWidth: 120, // максимальная длинна строки
